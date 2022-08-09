3c92e6d6342544ca70f514f3a59fc766db73a84c
duwonKim <dev.dothe1@gmail.com>
2022-08-09 18:51:30 +0900

    20220809-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
-import ToDoList from "./toDoList";
+import ToDoList from "./components/toDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
